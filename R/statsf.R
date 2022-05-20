d09bb605394127b2ea1b03955d99de982c3e1da9
ccebra <56567704+ccebra@users.noreply.github.com>
2022-05-20 05:45:35 -0500

    Also removes blank station codes that are not null

--- R/statsf.R
+++ R/statsf.R
@@ -3,6 +3,7 @@ statsf <- function(df){
   chipts
   chipts <- chipts[is.na(chipts$station_id) == 0,]
   chipts <- chipts[chipts$station_id != 0,]
+  chipts <- chipts[chipts$station_id != ""] # Two ways of storing no station for ebikes
   chipts <- chipts[is.na(chipts$lat) == 0,]
   chipts$pctelectric <- chipts$electric / chipts$total
   pts_sf = sf::st_as_sf(as.data.frame(chipts), coords = c('long','lat'),crs="WGS84")
