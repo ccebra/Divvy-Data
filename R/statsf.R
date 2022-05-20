e99e1d40232ff78d52e407a163d59d2b5e9ec54a
ccebra <56567704+ccebra@users.noreply.github.com>
2022-05-20 05:41:02 -0500

    Now removes null rows

--- R/statsf.R
+++ R/statsf.R
@@ -2,6 +2,7 @@ statsf <- function(df){
   chipts <- df
   chipts
   chipts <- chipts[is.na(chipts$station_id) == 0,]
+  chipts <- chipts[chipts$station_id != 0,]
   chipts <- chipts[is.na(chipts$lat) == 0,]
   chipts$pctelectric <- chipts$electric / chipts$total
   pts_sf = sf::st_as_sf(as.data.frame(chipts), coords = c('long','lat'),crs="WGS84")
